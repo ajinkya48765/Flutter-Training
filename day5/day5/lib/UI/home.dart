e244c0ead1dbc0d45b3c2c827e3b775f015f4756
Ajinkya S Khandave <ajinkyaakap11@gmail.com>
2020-08-02 01:10:55 +0530

    audio-player

--- day5/day5/lib/UI/home.dart
+++ day5/day5/lib/UI/home.dart
@@ -1,15 +1,25 @@
+
+
 import 'package:flutter/material.dart';
 
 myApp(){
-  var funTitle = Text("My Card", textAlign: TextAlign.center);
+  var funTitle = Text("My Card",
+   textAlign: TextAlign.center ,
+   style: TextStyle(color: Colors.black),
+   );
+
+  var leadicon = Icon(Icons.menu, color: Colors.black);
 
   var funappBar = AppBar(
     title: funTitle,
+    leading: leadicon,
     backgroundColor: Colors.amber.shade400,
   );
-
+ 
   var funScaffold = Scaffold(
     appBar: funappBar,
+    body: Image.network("https://raw.githubusercontent.com/ajinkya48765/Flutter-Training/master/risewhite.png",
+    color: Colors.grey,),
   );
 
   return MaterialApp(home: funScaffold);
